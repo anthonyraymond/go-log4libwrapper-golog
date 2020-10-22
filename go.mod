ce8613aca79c6c76fd81cffb8615fdc12354d290
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2020-10-22 17:30:30 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/anthonyraymond/go-log4libwrapper-golog
+
+go 1.15
+
+require (
+	github.com/anthonyraymond/go-log4lib v1.0.0
+	github.com/kataras/golog v0.1.5
+	golang.org/x/sys v0.0.0-20201020230747-6e5568b54d1a // indirect
+)
---
+++ log4libwrapper.go
@@ -0,0 +1,57 @@
+package log4libwrapper
+
+import (
+	"github.com/anthonyraymond/go-log4lib"
+	"github.com/kataras/golog"
+)
+
+type gologLoggerWrapper struct {
+	delegate *golog.Logger
+}
+
+func (w *gologLoggerWrapper) Info(args ...interface{}) {
+	w.delegate.Info(args)
+}
+
+func (w *gologLoggerWrapper) Warn(args ...interface{}) {
+	w.delegate.Warn(args)
+}
+
+func (w *gologLoggerWrapper) Error(args ...interface{}) {
+	w.delegate.Error(args)
+}
+
+func (w *gologLoggerWrapper) Panic(args ...interface{}) {
+	w.delegate.Error(args)
+	panic(args)
+}
+
+func (w *gologLoggerWrapper) Fatal(args ...interface{}) {
+	w.delegate.Fatal(args)
+}
+
+
+func (w *gologLoggerWrapper) Infof(template string, args ...interface{}) {
+	w.delegate.Infof(template, args)
+}
+
+func (w *gologLoggerWrapper) Warnf(template string, args ...interface{}) {
+	w.delegate.Warnf(template, args)
+}
+
+func (w *gologLoggerWrapper) Errorf(template string, args ...interface{}) {
+	w.delegate.Errorf(template, args)
+}
+
+func (w *gologLoggerWrapper) Panicf(template string, args ...interface{}) {
+	w.delegate.Errorf(template, args)
+	panic(args)
+}
+
+func (w *gologLoggerWrapper) Fatalf(template string, args ...interface{}) {
+	w.delegate.Fatalf(template, args)
+}
+
+func WrapGologLogger(pointerToLogger *golog.Logger) log4lib.LibLogger {
+	return &gologLoggerWrapper{delegate: pointerToLogger}
+}
