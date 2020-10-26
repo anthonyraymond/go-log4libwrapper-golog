309b0399c817bfe50472d501e750093f20aef6bd
Anthony Raymond <anthonyraymond@users.noreply.github.com>
2020-10-26 14:37:54 +0100

    Update log4libwrapper.go

--- log4libwrapper.go
+++ log4libwrapper.go
@@ -10,46 +10,46 @@ type gologLoggerWrapper struct {
 }
 
 func (w *gologLoggerWrapper) Info(args ...interface{}) {
-	w.delegate.Info(args)
+	w.delegate.Info(args...)
 }
 
 func (w *gologLoggerWrapper) Warn(args ...interface{}) {
-	w.delegate.Warn(args)
+	w.delegate.Warn(args...)
 }
 
 func (w *gologLoggerWrapper) Error(args ...interface{}) {
-	w.delegate.Error(args)
+	w.delegate.Error(args...)
 }
 
 func (w *gologLoggerWrapper) Panic(args ...interface{}) {
-	w.delegate.Error(args)
+	w.delegate.Error(args...)
 	panic(args)
 }
 
 func (w *gologLoggerWrapper) Fatal(args ...interface{}) {
-	w.delegate.Fatal(args)
+	w.delegate.Fatal(args...)
 }
 
 
 func (w *gologLoggerWrapper) Infof(template string, args ...interface{}) {
-	w.delegate.Infof(template, args)
+	w.delegate.Infof(template, args...)
 }
 
 func (w *gologLoggerWrapper) Warnf(template string, args ...interface{}) {
-	w.delegate.Warnf(template, args)
+	w.delegate.Warnf(template, args...)
 }
 
 func (w *gologLoggerWrapper) Errorf(template string, args ...interface{}) {
-	w.delegate.Errorf(template, args)
+	w.delegate.Errorf(template, args...)
 }
 
 func (w *gologLoggerWrapper) Panicf(template string, args ...interface{}) {
-	w.delegate.Errorf(template, args)
+	w.delegate.Errorf(template, args...)
 	panic(args)
 }
 
 func (w *gologLoggerWrapper) Fatalf(template string, args ...interface{}) {
-	w.delegate.Fatalf(template, args)
+	w.delegate.Fatalf(template, args...)
 }
 
 func WrapGologLogger(pointerToLogger *golog.Logger) log4lib.LibLogger {
